655a937e7c1215bcebb28acf6b23305a4869a3f3
RubTheKid <you@example.com>
2022-10-27 11:40:17 -0300

    update

---
+++ Migrations/20221026211756_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome,ImagemUrl) values('Bebidas','Bebidas.jpg')");
+            mb.Sql("Insert into Categorias(Nome,ImagemUrl) values('Comidas','Comidas.jpg')");
+            mb.Sql("Insert into Categorias(Nome,ImagemUrl) values('Doces','doces.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ Migrations/20221027133731_PopulaProdutos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into produtos (nome,descricao,preco,imagemurl,estoque,datacadastro,categoriaid)" +
+                "values('Coca-Cola Diet','refrigerante diet 350ml',5.50,'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("insert into produtos (nome,descricao,preco,imagemurl,estoque,datacadastro,categoriaid)" +
+                "values('X burguer','x burguer',15.00,'xburguer.jpg',50,now(),2)");
+
+            mb.Sql("insert into produtos (nome,descricao,preco,imagemurl,estoque,datacadastro,categoriaid)" +
+                "values('brigadeiro','chocolate',3.00,'brigadeiro.jpg',50,now(),3)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from Produtos");
+        }
+    }
+}
